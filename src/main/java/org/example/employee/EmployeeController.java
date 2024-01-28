e500dcf8daa288017ffdf75be179a5dd1f300fbe
Leon Myburgh <leonjohan3@gmail.com>
2024-01-28 11:43:38 +1100

    quit after 2 hrs

--- src/main/java/org/example/employee/EmployeeController.java
+++ src/main/java/org/example/employee/EmployeeController.java
@@ -1,24 +1,44 @@
 package org.example.employee;
 
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import lombok.RequiredArgsConstructor;
+import java.util.concurrent.TimeUnit;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 //@CrossOrigin
 //@CrossOrigin(methods = {RequestMethod.DELETE, RequestMethod.GET, RequestMethod.OPTIONS, RequestMethod.POST})
+//@Slf4j
 public class EmployeeController {
 
+    private static final String ECS_CONTAINER_METADATA_URI_V4 = System.getenv("ECS_CONTAINER_METADATA_URI_V4");
+    private static final String ECS_AGENT_URI = System.getenv("ECS_AGENT_URI");
+
     private final Map<Integer, Employee> employees;
+    private final RestTemplate restTemplate;
+
+    public EmployeeController(final Map<Integer, Employee> employees, final RestTemplateBuilder restTemplateBuilder) {
+        this.employees = employees;
+        this.restTemplate = restTemplateBuilder.build();
+    }
 
     @GetMapping("/employees")
 //    @CrossOrigin
@@ -60,5 +80,37 @@ public class EmployeeController {
         } else {
             throw new EmployeeNotFoundException(id);
         }
+    }
+
+    @GetMapping("/my-ip")
+    String getMyIp() {
+        return restTemplate.getForObject("http://checkip.amazonaws.com", String.class);
+    }
+
+    @GetMapping(value = "/task", produces = APPLICATION_JSON_VALUE)
+    String getTask() {
+        return restTemplate.getForObject(ECS_CONTAINER_METADATA_URI_V4 + "/task", String.class);
+    }
+
+    @GetMapping(value = "/stats", produces = APPLICATION_JSON_VALUE)
+    String getStats() {
+        return restTemplate.getForObject(ECS_CONTAINER_METADATA_URI_V4 + "/stats", String.class);
+    }
+
+    @GetMapping(value = "/task/stats", produces = APPLICATION_JSON_VALUE)
+    String getTaskStats() {
+        return restTemplate.getForObject(ECS_CONTAINER_METADATA_URI_V4 + "/task/stats", String.class);
+    }
+
+    @GetMapping(value = "/task-protection/v1/state", produces = APPLICATION_JSON_VALUE)
+    String getTaskProtectionState() {
+        return restTemplate.getForObject(ECS_AGENT_URI + "/task-protection/v1/state", String.class);
+    }
+
+    @Scheduled(initialDelay = 2, fixedDelay = 30, timeUnit = TimeUnit.HOURS)
+    void quit() throws URISyntaxException {
+        final var requestHeaders = new HttpHeaders();
+        requestHeaders.add(CONTENT_TYPE, APPLICATION_JSON_VALUE);
+        restTemplate.exchange("http://localhost:8080/actuator/shutdown", POST, new HttpEntity<>(requestHeaders), String.class);
     }
 }
--- src/main/java/org/example/employee/EmployeeDirectoryApplication.java
+++ src/main/java/org/example/employee/EmployeeDirectoryApplication.java
@@ -2,8 +2,10 @@ package org.example.employee;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(proxyBeanMethods = false)
+@EnableScheduling
 public class EmployeeDirectoryApplication {
 
 	public static void main(String[] args) {
