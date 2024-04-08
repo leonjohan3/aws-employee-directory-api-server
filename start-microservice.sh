d503ae475774d082ef56396ffa0ba3eb0cc9d00a
Leon Myburgh <leonjohan3@gmail.com>
2024-04-08 18:56:35 +1000

    add exec in start-microservice.sh

--- start-microservice.sh
+++ start-microservice.sh
@@ -4,4 +4,4 @@ set -eu -o pipefail
 
 aws sts get-caller-identity
 
-java -Duser.home=/tmp -Xms384m -Xmx384m -Xlog:gc -cp app/BOOT-INF/classes:app/BOOT-INF/lib/* org.example.employee.EmployeeDirectoryApplication
+exec java -Duser.home=/tmp -Xms384m -Xmx384m -Xlog:gc -cp app/BOOT-INF/classes:app/BOOT-INF/lib/* org.example.employee.EmployeeDirectoryApplication
